1391f7d58fc122c3cc4fdf673690b7f0656ff153
Philipp Czyborra <pczyborra@googlemail.com>
2018-07-16 18:30:44 +0200

    Added pixel iterator, Added two-color-mode

--- ImageHeightField/src/NetzeUndRauschen.cpp
+++ ImageHeightField/src/NetzeUndRauschen.cpp
@@ -18,6 +18,7 @@
 #include <chrono>
 #include "cinder/Capture.h"
 #include <fstream>
+#include "cinder/params/Params.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -28,9 +29,15 @@ class NetzeUndRauschen : public App {
 	void    keyDown( KeyEvent event ) override;
 	void    draw() override;
 	void	setInitialColormatrix();
+	void	createRandomColorMap();
+	void setNumberOfColors(short);
+	void toggleSingleColor();
+	params::InterfaceGl	mParams;
+	int xRes = 4;
+	int yRes = 3;
+	int resolutionMultiplier = 4;
 
  private:
-	
 	CameraPersp		mCam;
 	CameraUi		mCamUi;
 
@@ -46,6 +53,7 @@ struct ColorHSV
 	double h = 0;
 	double s = 0;
 	double v = 0;
+	short hueNumber = 0;
 };
 
 struct ColorRGB
@@ -62,7 +70,21 @@ float randomBetween(float smallNumber, float bigNumber)
 }
 
 const int netSize = 12;
+
 ColorHSV colorsHSV[netSize][netSize];
+
+
+//std::vector<std::vector<ColorHSV>> colorsHSV;
+
+ColorRGB initialColormatrix[netSize][netSize];
+
+//std::vector<std::vector<ColorRGB>> initialColormatrix;
+
+//std::vector<std::vector<float>> colorDistanceMatrix;
+float colorDistanceMatrix[netSize][netSize];
+
+//float colorDistanceMatrix[netSize][netSize];
+
 vec2 pointMatrix[netSize][netSize];
 
 // strength of effect
@@ -74,10 +96,72 @@ float perlinShiftSpeed = 1.0;
 // saturation manipulator
 float saturationAddend = 0;
 
+float defaultHue = 242.0;
+
+float singleColorRatio = 0.2;
+
+long timeElapsed = 0;
+
 std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 
 Perlin perlinNoise;
 
+bool flagSingleColor = false;
+
+// number of different colors
+int numberOfHues = 1;
+int numberOfHuesNew = numberOfHues;
+
+void NetzeUndRauschen::toggleSingleColor() 
+{
+	flagSingleColor = !flagSingleColor;
+	createRandomColorMap();
+}
+
+void NetzeUndRauschen::setNumberOfColors(short number)
+{
+	numberOfHues = number;
+	createRandomColorMap();
+}
+
+void NetzeUndRauschen::createRandomColorMap()
+{
+	//colorsHSV.resize(xRes * resolutionMultiplier, std::vector<ColorHSV>((int)(yRes * resolutionMultiplier)));
+	//initialColormatrix.resize(xRes * resolutionMultiplier, std::vector<ColorRGB>((int)(yRes * resolutionMultiplier)));
+	//colorDistanceMatrix.resize(xRes * resolutionMultiplier, std::vector<float>((int)(yRes * resolutionMultiplier)));
+	numberOfHues = numberOfHuesNew;
+		for (int i = 0; i < netSize; i++) // TODO
+	{
+		for (int j = 0; j < netSize; j++)
+		{
+			pointMatrix[i][j].x = i;
+			pointMatrix[i][j].y = j;
+
+			//colorsHSV[i][j].h = defaultHue / 360; // unnoetig
+			//colorsHSV[i][j].s = randomBetween(.5, .85);
+			if ((flagSingleColor == false) || (numberOfHues > 1))
+			{
+				colorsHSV[i][j].s = randomBetween(.3, .65);
+				colorsHSV[i][j].v = randomBetween(.8, 1.0);
+				colorsHSV[i][j].hueNumber = (rand() * (int)(numberOfHues) / RAND_MAX);
+			}
+			else
+			{
+				float randomNumber = randomBetween(0, 1.0);
+				if (randomNumber > singleColorRatio)
+				{
+					colorsHSV[i][j].s = 0;
+				}
+				else
+				{
+					colorsHSV[i][j].s = randomBetween(.3, .65);
+				}
+				colorsHSV[i][j].v = randomBetween(.8, 1.0);
+				colorsHSV[i][j].hueNumber = (rand() * (int)(numberOfHues) / RAND_MAX);
+			}
+		}
+	}
+}
 
 void NetzeUndRauschen::setup()
 {
@@ -87,14 +171,33 @@ void NetzeUndRauschen::setup()
 	gl::enableDepthWrite();    
 
 
+	mParams = params::InterfaceGl("Parameters", ivec2(220, 170));
+
+	mParams.addParam("Effect strength", &perlinEffectStrength, "min=0.0 max=2.0 step=0.1 keyIncr=p keyDecr=P");
+	mParams.addParam("Perlin shift speed", &perlinShiftSpeed, "min=0.5 max=4.0 step=0.25 keyIncr=p keyDecr=P");
+	mParams.addParam("Saturation addend", &saturationAddend, "min=0.0 max=0.5 step=0.02 keyIncr=p keyDecr=P");
+	mParams.addParam("Default hue", &defaultHue, "min=0.0 max=360 step=1 keyIncr=p keyDecr=P");
+	mParams.addParam("Number of Colors", &numberOfHuesNew, "min=1 max=16 step=1 keyIncr=p keyDecr=P");
+	mParams.addButton("Refresh net", [&]() { createRandomColorMap(); });
+	mParams.addButton("Single-Color mode", [&]() { toggleSingleColor(); });
+	mParams.addParam("Single color ratio", &singleColorRatio, "min=0 max=1 step=0.1 keyIncr=p keyDecr=P");
+
 	mCapture = Capture::create(640, 480);
 	mCapture->start();
 
+
+
+	mCam = ci::CameraPersp();
 	mCamUi = CameraUi( &mCam, getWindow() );
 	mCam.setNearClip( 1 );
 	mCam.setFarClip( 2000 );
-	mCam.lookAt(vec3(0, netSize / 2, netSize / 2));
-	mCam.setEyePoint(vec3(15, netSize / 2, netSize /  2));
+	//mCam.lookAt(vec3(0, netSize / 2, netSize / 2));
+	//mCam.setEyePoint(vec3(15, netSize / 2, netSize /  2));
+
+
+	mCam.setEyePoint(vec3(6, 6, 10));
+	mCam.lookAt(vec3(6, 6, 0));
+
 
 
 	srand(time(NULL));
@@ -102,30 +205,15 @@ void NetzeUndRauschen::setup()
 	Surface initialCameraScan = *mCapture->getSurface();
 	setInitialColormatrix();
 
-	for (int i = 0; i < netSize; i++)
-	{
-		for (int j = 0; j < netSize; j++)
-		{
-			pointMatrix[i][j].x = i;
-			pointMatrix[i][j].y = j;
-
-			colorsHSV[i][j].h = 242.0 / 360.0;
-			//colorsHSV[i][j].s = randomBetween(.5, .85);
-			colorsHSV[i][j].s = randomBetween(.3, .65);
-			colorsHSV[i][j].v = randomBetween(.8, 1.0);
-
-		}
-	}
+	createRandomColorMap();
 
 }
 
 
-ColorRGB initialColormatrix[netSize][netSize];
-
-float colorDistanceMatrix[netSize][netSize];
 
 void NetzeUndRauschen::setInitialColormatrix()
 {
+	//initialColormatrix.resize(xRes * resolutionMultiplier, std::vector<ColorRGB>((int)(yRes * resolutionMultiplier)));
 	for (int i = 0; i < netSize; i++)
 	{
 		for (int j = 0; j < netSize; j++)
@@ -215,10 +303,23 @@ void NetzeUndRauschen::keyDown( KeyEvent event )
 
 std::chrono::high_resolution_clock::time_point timePrev;
 
+
+std::chrono::high_resolution_clock::time_point time1 =  std::chrono::high_resolution_clock::now();
+std::chrono::high_resolution_clock::time_point time2;
+long millis = 0;
+
 void NetzeUndRauschen::draw()
 {
+	gl::clear();
+
+	gl::setMatrices(mCam);
+
+	gl::drawCoordinateFrame();
+	gl::translate(vec3(-netSize * 0.4f, -netSize * 0.5f, 0.0f));
 	ColorRGB colormatrix[netSize][netSize];
 		 
+
+
 	for (int i = 0; i < netSize; i++)
 	{
 		for (int j = 0; j < netSize; j++)
@@ -229,6 +330,13 @@ void NetzeUndRauschen::draw()
 		}
 	}
 
+
+	//while (pixelIter.line())
+	//{
+
+	//}
+
+	/*
 	for (int i = 0; i < netSize; i++)
 	{
 		for (int j = 0; j < netSize; j++)
@@ -245,6 +353,30 @@ void NetzeUndRauschen::draw()
 			}
 		}
 	}
+	*/
+
+
+	Area area(0, 0, 480, 480);
+	int xCount = 640 / (resolutionMultiplier * xRes); // TODO
+	int yCount = 480 / (resolutionMultiplier * yRes);
+	Surface surface = *mCapture->getSurface();
+	Surface::Iter pixelIter = surface.getIter(area);
+	vec2 pixelIterPos(0, 0);
+	while (pixelIter.line())
+	{
+		while (pixelIter.pixel())
+		{
+			float r = pixelIter.r();
+			float g = pixelIter.g();
+			float b = pixelIter.b();
+			pixelIterPos = pixelIter.getPos();
+			colormatrix[(int)floor(pixelIterPos).x / 40][(int)floor(pixelIterPos).y / 40].r += r / (pow((480 / netSize), 2)  * 255);
+			colormatrix[(int)floor(pixelIterPos).x / 40][(int)floor(pixelIterPos).y / 40].g += g / (pow((480 / netSize), 2) * 255);
+			colormatrix[(int)floor(pixelIterPos).x / 40][(int)floor(pixelIterPos).y / 40].b += b / (pow((480 / netSize), 2) * 255);
+		}
+	}
+
+
 
 	for (int i = 0; i < netSize; i++)
 	{
@@ -259,15 +391,22 @@ void NetzeUndRauschen::draw()
 	gl::setMatrices( mCam );
 	
 
+	time1 = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double, std::milli> time1time2 = time1 - time2;
+	millis += time1time2.count() * perlinShiftSpeed;
+	time2 = std::chrono::high_resolution_clock::now();
+
 	std::chrono::high_resolution_clock::time_point timeNow = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double, std::milli> timeSpan = timeNow - startTime;
+	timeElapsed += (timeSpan.count() - timeElapsed) * perlinShiftSpeed;
 
 	for (int i = 0; i < netSize; i++)
 	{
 		for (int j = 0; j < netSize; j++)
 		{
-			pointMatrix[i][j].x = i + perlinEffectStrength * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] * perlinNoise.noise((timeSpan.count() * perlinShiftSpeed / 1000 + i), (timeSpan.count() * perlinShiftSpeed / 1000 + j)) -.5;
-			pointMatrix[i][j].y = j + perlinEffectStrength * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] * perlinNoise.noise((timeSpan.count() * perlinShiftSpeed / 1000 + i) + .5, (timeSpan.count() * perlinShiftSpeed / 1000 + j) + .5) - .5;
+			pointMatrix[i][j].x = i + perlinEffectStrength * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] * perlinNoise.noise(((double)millis / 1000 + i), (timeSpan.count() * perlinShiftSpeed / 1000 + j)) -.5;
+			pointMatrix[i][j].y = j + perlinEffectStrength * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] * perlinNoise.noise(((double)millis / 1000 + i) + .5, (timeSpan.count() * perlinShiftSpeed / 1000 + j) + .5) - .5;
 	
 		}
 	}
@@ -291,7 +430,14 @@ void NetzeUndRauschen::draw()
 			}
 			else
 			{
-				gl::color(cinder::Color(cinder::ColorModel::CM_HSV, colorsHSV[i][j].h, colorsHSV[i][j].s + hsvColor.get(CM_HSV).x * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] + saturationAddend, colorsHSV[i][j].v));
+				float hue = (defaultHue/360) + (float)colorsHSV[i][j].hueNumber / (float)numberOfHues - floor((defaultHue / 360) + (float)colorsHSV[i][j].hueNumber / (float)numberOfHues);
+				
+				if (flagSingleColor == false || numberOfHues > 1)
+					gl::color(cinder::Color(cinder::ColorModel::CM_HSV, hue, colorsHSV[i][j].s + hsvColor.get(CM_HSV).x * colorDistanceMatrix[netSize - 1 - i][netSize - 1 - j] + saturationAddend, colorsHSV[i][j].v));
+				else
+				{
+					gl::color(cinder::Color(cinder::ColorModel::CM_HSV, hue, colorsHSV[i][j].s, colorsHSV[i][j].v));
+				}
 			}
 			if ((i + 1 < netSize) && (j + 1 < netSize))
 			gl::drawSolidTriangle(pointMatrix[i][j], pointMatrix[i][j + 1], pointMatrix[i + 1][j]);
@@ -301,6 +447,8 @@ void NetzeUndRauschen::draw()
 			gl::drawSolidTriangle(pointMatrix[i][j], pointMatrix[i][j - 1], pointMatrix[i - 1][j]);
 		}
 	}
+
+	mParams.draw();
 }
 
 
